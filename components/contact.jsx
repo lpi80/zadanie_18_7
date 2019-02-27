8712ea2572a0964f02a67d492e2a5001549ae574
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-02-27 09:11:54 +0100

    Clear code

--- components/contact.jsx
+++ components/contact.jsx
@@ -3,7 +3,7 @@ Contact = React.createClass({
   render: function () {
     return (
       <div className={'contactItem'}>
-        <img className={'contactImage'} src={'http://chittagongit.com//images/contacts-icon-png/contacts-icon-png-8.jpg'} />
+        <img className={'contactImage'} src={'components/kontakt.png'} />
         <p className={'contactLabel'}>{this.props.item.firstName}</p>
         <p className={'contactLabel'}>{this.props.item.lastName}</p>
         <a className={'contactEmail'} href={this.props.item.email}>{this.props.item.email}</a>
